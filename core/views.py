635ff0817a31eef6ac2a59322e3b53eefdf02e3f
Sávio Vasconcelos <plosavio.sv@gmail.com>
2022-11-21 15:15:35 -0300

    pagina de listagem

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from core.models import Evento
 # Create your views here.
+
+def lista_eventos(request):
+    dados = {}
+    usuario = request.user
+    dados['db'] = Evento.objects.filter(usuario=usuario)
+    return render(request,'agenda.html',dados)
